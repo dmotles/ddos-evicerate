e99cf5633e1d05fc610e54bda92f40c8c03d5b50
Daniel Motles <seltom.dan@gmail.com>
2013-01-02 16:51:54 -0500

    moved functions around to prepare to support a wide multitude of inputs
    I'm making classes to better modularize netstat scrapers

--- ddos.py
+++ ddos.py
@@ -3,43 +3,77 @@
 import subprocess
 import re
 
+"""*nix netstat scraper implementation(s)"""
+
 class NetstatException(Exception):
+    """
+    Thrown whenever there's a netstat issue.
+    """
     pass
 
-def netstat_cmd():
-    return [ 'netstat' ]
+
+class Netstat:
+    """
+    An abstract class that will define netstat scraper implementation.
+
+    Extend this class to provide netstat scraper support on a per-OS basis.
+    """
+
+    def get_output(self):
+        """Returns a string of output from netstat command (or equivelant)."""
+        raise NotImplementedError("netstat should be implemented!")
 
 
-def netstat_args():
-    return [ '-ntu' ]
+    def parse(self,ouput):
+        """
+        Returns a list of tuples that contain current active connections.
+
+        Tuple format should be (proto,destip,destport,srcip,srcport,state).
+        """
+        raise NotImplementedError("parse_netstat should be implemented!")
 
 
-def netstat_pattern():
-    strpat = r'^(tcp|udp)\s*\d+\s*\d+\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-    strpat += r':(\d{1,5})\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})\s*'
-    strpat += r'([a-zA-Z_]+)\s*$'
-    return strpat
+class RhelNetstat(Netstat):
+    """
+    An implementation of the netstat scraper for CentOS/RHEL.
+
+    This is the default class.
+    """
+
+    def cmd(self):
+        return [ 'netstat' ]
 
 
-def parse_netstat(output):
-    lines = output.split("\n")
-    pattern = re.compile(netstat_pattern(),re.MULTILINE)
-    connList = []
-    for line in lines:
-        match = pattern.match(line)
-        if match != None:
-            connList.append( match.groups() )
-    return connList
+    def args(self):
+        return [ '-ntu' ]
 
 
-def netstat():
-    args = netstat_cmd() + netstat_args()
-    proc = subprocess.Popen( args, stdout=subprocess.PIPE )
-    output = proc.communicate()[0]  # 0=stdout 1=stderr
-    proc.wait()
-    if( proc.returncode != 0 ):
-        raise NetstatException( 'Netstat didn\'t return 0' )
-    return output
+    def pattern(self):
+        strpat = r'^(tcp|udp)\s*\d+\s*\d+\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        strpat += r':(\d{1,5})\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})'
+        strpat += r'\s*([a-zA-Z_]+)\s*$'
+        return strpat
+
+
+    def parse(self,output):
+        lines = output.split("\n")
+        pattern = re.compile(self.pattern(),re.MULTILINE)
+        connList = []
+        for line in lines:
+            match = pattern.match(line)
+            if match != None:
+                connList.append( match.groups() )
+        return connList
+
+
+    def get_output(self):
+        args = self.cmd() + self.args()
+        proc = subprocess.Popen( args, stdout=subprocess.PIPE )
+        output = proc.communicate()[0]  # 0=stdout 1=stderr
+        proc.wait()
+        if( proc.returncode != 0 ):
+            raise NetstatException( 'Netstat didn\'t return 0' )
+        return output
 
 
 def print_connlist(connlist):
@@ -50,7 +84,10 @@ def print_connlist(connlist):
 
 
 def main():
-    print_connlist(parse_netstat(netstat()))
+    netstat = RhelNetstat()
+    output = netstat.get_output()
+    connlist = netstat.parse(output)
+    print_connlist(connlist)
     return 0
 
 
