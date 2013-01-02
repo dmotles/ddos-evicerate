6c4760f8b15bd12adda53d526a4a6a57cd0b5245
Daniel Motles <seltom.dan@gmail.com>
2013-01-02 16:13:59 -0500

    parses netstat and prints output correctly.

--- ddos.py
+++ ddos.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 
 import subprocess
+import re
 
 class NetstatException(Exception):
     pass
@@ -13,6 +14,24 @@ def netstat_args():
     return [ '-ntu' ]
 
 
+def netstat_pattern():
+    strpat = r'^(tcp|udp)\s*\d+\s*\d+\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+    strpat += r':(\d{1,5})\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d{1,5})\s*'
+    strpat += r'([a-zA-Z_]+)\s*$'
+    return strpat
+
+
+def parse_netstat(output):
+    lines = output.split("\n")
+    pattern = re.compile(netstat_pattern(),re.MULTILINE)
+    connList = []
+    for line in lines:
+        match = pattern.match(line)
+        if match != None:
+            connList.append( match.groups() )
+    return connList
+
+
 def netstat():
     args = netstat_cmd() + netstat_args()
     proc = subprocess.Popen( args, stdout=subprocess.PIPE )
@@ -23,8 +42,15 @@ def netstat():
     return output
 
 
+def print_connlist(connlist):
+    fmtstr = '%5s %15s %15s %15s %15s %15s'
+    print fmtstr % ('proto','destip','destport','srcip','srcport','state')
+    for conn in connlist:
+        print fmtstr % conn
+
+
 def main():
-    print netstat()
+    print_connlist(parse_netstat(netstat()))
     return 0
 
 
